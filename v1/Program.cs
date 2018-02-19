821f7bf60c8c06642944a7c9fc4f4795750a9f29
Austin Wise <AustinWise@gmail.com>
2018-02-19 03:58:56 +0000

    Add v1.1 version, showing that it does not crash.

---
+++ v1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp2
+{
+    class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+    delegate TResult MyFunc<in T, out TResult>(T arg);
+    static class Program
+    {
+        static string GetEmail(LoginViewModel model)
+        {
+            return model.Email;
+        }
+        static bool GetRememberMe(LoginViewModel model)
+        {
+            return model.RememberMe;
+        }
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Starting crash repro.");
+            Console.Out.Flush();
+
+            MyFunc<LoginViewModel, string> a = GetEmail;
+            try
+            {
+                a(null);
+            }
+            catch
+            {
+            }
+            MyFunc<LoginViewModel, bool> b = GetRememberMe;
+            try
+            {
+                b(null);
+            }
+            catch
+            {
+            }
+
+            Console.WriteLine("Did not crash.");
+            Console.Out.Flush();
+            return 0;
+        }
+    }
+}
+
