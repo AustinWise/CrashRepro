939390e72d4afe4b48665bbd3f494543d2529219
Austin Wise <AustinWise@gmail.com>
2018-03-06 22:17:37 -0800

    Working on reproduction.

--- c/test.c
+++ c/test.c
@@ -3,44 +3,60 @@
 #include <signal.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <ucontext.h>
 #include <sys/mman.h>
 
 #define MAPPED_MEMORY_SIZE 4096
-#define MY_MSG "in handler\n"
 
-static int *mapped_memory;
+static int *mapped_memory1;
+static int *mapped_memory2;
 static void *alt_signal_stack;
 
-static void segv_handler(int code, siginfo_t *siginfo, void *context)
+static void change_protection_to_readable(void* addr)
 {
 	int rc;
 
-	void *addr = siginfo->si_addr;
-
-	if (addr != mapped_memory)
+	if (addr != mapped_memory1 && addr != mapped_memory2)
 	{
 		//This was not the address we were expecting.
 		abort();
 	}
 
-	rc = mprotect(mapped_memory, MAPPED_MEMORY_SIZE, PROT_READ);
+	rc = mprotect(addr, MAPPED_MEMORY_SIZE, PROT_READ);
 	if (rc != 0)
 	{
 		abort();
 	}
+}
 
-	write(STDERR_FILENO, MY_MSG, sizeof(MY_MSG));
+static void segv_handler(int code, siginfo_t *siginfo, void *context)
+{
+	//ucontext_t* fault_context = (ucontext_t*)context;
+	//int* rax = (int*)fault_context->uc_mcontext.gregs[REG_RAX];
+	void *addr = siginfo->si_addr;
+	change_protection_to_readable(addr);
+	//setcontext(fault_context);
+}
+
+static void doRead(int id, int *addr)
+{
+	printf("reading from mapped memory %d: %d\n", id, *addr);
 }
 
 int main(int argc, char **argv)
 {
 	int rc;
 
-	mapped_memory = mmap(NULL, MAPPED_MEMORY_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if (mapped_memory == MAP_FAILED)
+	mapped_memory1 = mmap(NULL, MAPPED_MEMORY_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (mapped_memory1 == MAP_FAILED)
 	{
-		perror("mmap");
+		perror("mmap1");
+		return EXIT_FAILURE;
+	}
+	mapped_memory2 = mmap(NULL, MAPPED_MEMORY_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (mapped_memory2 == MAP_FAILED)
+	{
+		perror("mmap2");
 		return EXIT_FAILURE;
 	}
 
@@ -63,9 +79,9 @@ int main(int argc, char **argv)
 	}
 
 	struct sigaction myNewAction;
-	memset(&myNewAction, 0, sizeof(myNewAction));
+    myNewAction.sa_handler = NULL;
 	myNewAction.sa_sigaction = segv_handler;
-	myNewAction.sa_flags = SA_SIGINFO | SA_ONSTACK;
+	myNewAction.sa_flags = SA_RESTART | SA_SIGINFO | SA_ONSTACK;
 	rc = sigaction(SIGSEGV, &myNewAction, NULL);
 	if (rc != 0)
 	{
@@ -73,7 +89,8 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	printf("reading from mapped memory: %d\n", *mapped_memory);
+	doRead(1, mapped_memory1);
+	doRead(2, mapped_memory2);
 
 	puts("I am still alive!?");
 
